bc0ef8253b4a3a83d3c34121e366edb93152f078
Kevin Choi <kevin.choi@yale.edu>
2019-08-13 14:42:37 -0400

    Finished basic functionality of draw screen

--- screens/Draw.js
+++ screens/Draw.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Animated } from 'react-native';
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	Animated,
+	AsyncStorage
+} from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
-import { getUniqueID } from 'react-native-device-info';
 import styles from '../assets/styles';
+import { SCREENS } from '../constants';
 
 class Draw extends Component {
 	constructor(props) {
@@ -23,13 +29,38 @@ class Draw extends Component {
 				if (this.state.timer === 1) {
 					// When time runs out, go to list screen
 					clearInterval(this.interval);
-					console.log('hi');
+					// Save the new recording
+					const record = {
+						record: this.state.record,
+						time: this.state.startTime
+					};
+					AsyncStorage.getItem('record')
+						.then(recordList => {
+							// Get current list of recordings
+							if (!recordList) {
+								recordList = [];
+							} else {
+								recordList = JSON.parse(recordList);
+							}
+							// Add new recording and save
+							recordList.push(record);
+							console.log(recordList);
+							return AsyncStorage.setItem(
+								'record',
+								JSON.stringify(recordList)
+							);
+						})
+						// Navigate to list screen
+						.then(() =>
+							this.props.navigation.navigate(SCREENS.LIST)
+						)
+						.catch(err => console.log(err));
 				} else {
 					// Update timer
 					this.setState({ timer: this.state.timer - 1 });
 				}
 			}, 1000);
-			// Set start time for record
+			// Set start time for recording
 			this.setState({ startTime: event.nativeEvent.timestamp });
 		}
 		// Track the initial input as well
@@ -37,7 +68,6 @@ class Draw extends Component {
 	};
 
 	trackRecord = event => {
-		console.log(this.state.record);
 		// Get current touch coordinates
 		const posX = event.nativeEvent.pageX;
 		const posY = event.nativeEvent.pageY;
@@ -50,7 +80,7 @@ class Draw extends Component {
 			}
 		}
 		let points = this.state.points.slice(pruneIndex);
-		// Add a new point to current screen and record
+		// Add a new point to current screen and recording
 		points.push({
 			posX,
 			posY,
@@ -66,7 +96,7 @@ class Draw extends Component {
 					: 0
 			}
 		];
-		// Save the current screen and record
+		// Save the current screen and recording
 		this.setState(
 			{
 				points,
@@ -84,8 +114,6 @@ class Draw extends Component {
 			}
 		);
 	};
-
-	stopRecord = event => {};
 
 	render() {
 		return (
