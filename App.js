0ed5f7b5a19b24de6e06982a1e5f60a848e71b9a
Kevin Choi <kevin.choi@yale.edu>
2019-08-15 10:41:17 -0400

    Removed all headers

--- App.js
+++ App.js
@@ -1,32 +1,17 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { DrawScreen, EventScreen, ListScreen } from './screens';
 import { SCREENS } from './constants';
-import { MAIN_BLUE } from './assets/colorScheme';
-import styles from './assets/styles';
-
-const navigationOptions = {
-	headerStyle: styles.header,
-	headerTintColor: MAIN_BLUE
-};
 
 const Navigator = createStackNavigator(
 	{
-		Draw: {
-			screen: DrawScreen,
-			navigationOptions: {
-				header: null
-			}
-		},
-		Event: {
-			screen: EventScreen,
-			navigationOptions
-		},
-		List: {
-			screen: ListScreen,
-			navigationOptions
-		}
+		Draw: DrawScreen,
+		Event: EventScreen,
+		List: ListScreen
 	},
-	{ initialRouteName: SCREENS.DRAW }
+	{
+		initialRouteName: SCREENS.DRAW,
+		headerMode: 'none'
+	}
 );
 
 export default createAppContainer(Navigator);
